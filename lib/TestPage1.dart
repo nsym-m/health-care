653f4c35c058e0d3456ac24716c816c382810d44
nishiyama <mickey-ya@outlook.jp>
2022-03-09 00:59:33 +0900

    Add: routingのサンプル

---
+++ lib/TestPage1.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:health_care/TestPage2.dart';
+
+class TestPage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Test1"),
+        ),
+        body: Center(
+            child: TextButton(
+                onPressed: () => {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return TestPage2();
+                      }))
+                    },
+                child: Text("進む", style: TextStyle(fontSize: 80)))));
+  }
+}
---
+++ lib/TestPage2.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:health_care/TestPage3.dart';
+
+class TestPage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Test2"),
+        ),
+        body: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          TextButton(
+              onPressed: () => {
+                    Navigator.of(context)
+                        .push(MaterialPageRoute(builder: (context) {
+                      return TestPage3();
+                    }))
+                  },
+              child: Text("進む", style: TextStyle(fontSize: 80))),
+          TextButton(
+              onPressed: () => {Navigator.of(context).pop()},
+              child: Text("戻る", style: TextStyle(fontSize: 80)))
+        ])));
+  }
+}
