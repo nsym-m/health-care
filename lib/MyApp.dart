b54e2b883f301976e57ba676d9a0e4ca8d7be632
nishiyama <mickey-ya@outlook.jp>
2022-04-10 20:42:13 +0900

    Update: ファイル分割

---
+++ lib/MyApp.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:health_care/MyHomePage.dart';
+import 'package:health_care/TestPage1.dart';
+import 'package:health_care/TestPage2.dart';
+import 'package:health_care/HealthApp.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Deaaamo Homeああああ Page  aa'),
+        routes: {
+          "/test1": (BuildContext context) => TestPage1(),
+          "/test2": (BuildContext context) => TestPage2(),
+          "/test3": (BuildContext context) => HealthApp(),
+        });
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:health_care/MyApp.dart';
 
-import 'package:health_care/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
